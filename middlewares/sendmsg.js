32677e305a60d706a48419d37dd2a3fc615a9ed8
dollartree22 <railwayg1122@gmail.com>
2025-09-16 16:52:36 +0500

    Update sendmsg.js
    add twilio

--- middlewares/sendmsg.js
+++ middlewares/sendmsg.js
@@ -1,40 +1,16 @@
-const nodemailer = require('nodemailer');
+// middlewares/sendmsg.js
 const twilio = require("twilio");
-// const myemail = 'onedollartreestore@gmail.com';
-// const mypass = 'woganmuloxbyoopf'; 
-// const myemail = process.env.SMTP_USER;
-// const mypass = process.env.SMTP_PASS;
 
 const client = twilio(
   process.env.TWILIO_ACCOUNT_SID,
   process.env.TWILIO_AUTH_TOKEN
 );
 
-// configure nodemailer transport
-// const transporter = nodemailer.createTransport({
-//   host: "smtp.gmail.com",
-//   port: 465,
-//   secure: true, // use SSL
-//   auth: {
-//     user: myemail,
-//     pass: mypass,
-//   },
-// });
-
-// const sendmsg = (msg, email, subject) => {
-//   return new Promise((resolve, reject) => {
-//     const mailOptions = {
-//       from: myemail,
-//       to: email,
-//       subject,
-//       html: msg,
-//     };
-
-    async function sendmsg(to, otp) {
+async function sendmsg(to, otp) {
   try {
     const message = await client.messages.create({
       from: process.env.TWILIO_WHATSAPP_FROM,   // Twilio sandbox number
-      to: `whatsapp:${to}`,                     // user کا WhatsApp نمبر (+92322....)
+      to: `whatsapp:${to}`,                     // User کا WhatsApp نمبر (+92322....)
       body: `Your OTP code is: ${otp}`,
     });
 
@@ -42,22 +18,8 @@ const client = twilio(
     return true;
   } catch (error) {
     console.error("Error sending OTP:", error);
-    return false;
+    throw new Error("OTP sending failed");
   }
 }
 
-    transporter.sendMail(mailOptions, function (error, info) {
-      if (error) {
-        reject(error);
-      } else {
-        console.log('Email sent: ' + info.response);
-        resolve(true);
-        console.log("SMTP_USER:", myemail);
-        console.log("SMTP_PASS:", mypass ? "FOUND" : "NOT FOUND");
-      }
-    });
-  });
-};
 module.exports = sendmsg;
-
-
