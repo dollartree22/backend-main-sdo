523fb5231a3ae34aa1db12a96d54ef82db187092
dollartree22 <railwayg1122@gmail.com>
2025-09-16 17:00:58 +0500

    Update server.js
    udpate

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
 require("dotenv").config();
-const app=require('./app.js')
-const port=process.env.PORT ||80;
+const app = require('./app.js');
+const port = process.env.PORT || 80;
+
 // Handling Uncaught Exception
 process.on("uncaughtException", (err) => {
-    console.log(`Error: ${err.message}`);
-    console.log(`Shutting down the server due to Uncaught Exception`);
-    process.exit(1);
-  });
-app.get('/' ,(req,res)=>{
-    res.send('app is working')
-})
-app.listen(port,()=>{
-    console.log(`App is running at http://localhost:${port}`);
-    
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Uncaught Exception`);
+  process.exit(1);
 });
+
+// Root test route
+app.get('/', (req, res) => {
+  res.send('app is working');
+});
+
+// ✅ Save server instance
+const server = app.listen(port, () => {
+  console.log(`App is running at http://localhost:${port}`);
+});
+
 // Unhandled Promise Rejection
 process.on("unhandledRejection", (err) => {
-    console.log(`Error: ${err}`);
-    console.log(`Shutting down the server due to Unhandled Promise Rejection`);
-  
-    server.close(() => {
-      process.exit(1);
-    });
+  console.log(`Error: ${err}`);
+  console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+
+  server.close(() => {
+    process.exit(1);
   });
+});
